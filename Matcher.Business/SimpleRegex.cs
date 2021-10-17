e08c8242fb839e6a2495aff080fc1f0b3d802cdd
Aghost <youri_van_sorgen2@example.com>
2021-10-17 14:45:33 +0200

    add comments

--- Matcher.Business/SimpleRegex.cs
+++ Matcher.Business/SimpleRegex.cs
@@ -5,20 +5,29 @@ namespace Matcher.Business
     public class SimpleRegex
     {
         public static bool isMatch(string s, string p) {
+            // Create 2d "truth" table, + 1 extra row and column for the results
             bool[][] bArr = new bool[s.Length + 1][];
 
             for (int i = 0; i < bArr.Length; i++) {
                 bArr[i] = new bool[p.Length + 1];
             }
 
+            // Set "" == "" to true
             bArr[0][0] = true;
 
+            // if previous == '*', set current bool to previous previous value
             for (int i = 1; i < bArr[0].Length; i++) {
                 if (p[i - 1] == '*') {
                     bArr[0][i] = bArr[0][i - 2];
                 }
             }
 
+            // loop over truth table (skip first row)
+            // if we encounter a '.' or char is the same, current bool = previous(j-1, i-1) result
+            //  if we encounter a '*', then current bool = previous(j - 2) character's result and
+            //      if previous previous character was a '.' or it was equal to previous character:
+            //          current character = true if current character is true, or previous(i-1) character was true
+            // else the current character is false.
             for (int i = 1; i < bArr.Length; i++) {
                 for (int j = 1; j < bArr[0].Length; j++) {
                     if (p[j - 1] == '.' || p[j - 1] == s[i - 1]) {

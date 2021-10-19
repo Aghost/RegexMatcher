6799d5c956b0978b214a95815d24271a877457f5
Aghost <youri_van_sorgen2@example.com>
2021-10-19 20:45:09 +0200

    renamed vars, multidimensional arr to jagged

--- Matcher.Business/LevenshteinDistance.cs
+++ Matcher.Business/LevenshteinDistance.cs
@@ -8,22 +8,26 @@ namespace Matcher.Business
             if (s_from.Length == 0) { return s_to.Length ;}
             if (s_to.Length == 0) { return s_from.Length ;}
 
-            int n = s_from.Length;
-            int m = s_to.Length;
+            int from_len = s_from.Length;
+            int to_len = s_to.Length;
 
-            int[,] d = new int[n+1, m+1];
+            int[][] d = new int[from_len + 1][];
 
-            for (int i = 0; i <= n; d[i,0] = i++) { }
-            for (int j = 0; j <= m; d[0,j] = j++) { }
+            for (int i = 0; i < d.Length; i++) { d[i] = new int[to_len + 1]; }
 
-            for (int i = 1; i <= n; i++ ) {
-                for (int j = 1; j <= m; j++) {
+            for (int i = 0; i <= from_len; d[i][0] = i++) { }
+            for (int j = 0; j <= to_len; d[0][j] = j++) { }
+
+            for (int i = 1; i <= from_len; i++ ) {
+                for (int j = 1; j <= to_len; j++) {
                     // current_square = check lowest of (above value, left value, aboveleft + ((current_char_to == current_char_from)) ? 0 : 1)
-                    d[i,j] = Math.Min(Math.Min(d[i - 1, j] + 1, d[i , j - 1] + 1), d[i - 1, j - 1] + ((s_to[j - 1] == s_from[i - 1]) ? 0 : 1));
+                    d[i][j] = Math.Min(
+                            Math.Min(d[i - 1][j] + 1, d[i][j - 1] + 1),
+                            d[i - 1][j - 1] + ((s_to[j - 1] == s_from[i - 1]) ? 0 : 1));
                 }
             }
 
-            return d[n,m];
+            return d[from_len][to_len];
         }
     }
 }
--- Matcher.Business/SimpleRegex.cs
+++ Matcher.Business/SimpleRegex.cs
@@ -5,7 +5,8 @@ namespace Matcher.Business
     public class SimpleRegex
     {
         public static bool IsMatch(string s, string p) {
-            // Create 2d "truth" table, + 1 extra row and column for the results
+            // DYNAMIC
+            // Create 2d "truth" table
             bool[][] bArr = new bool[s.Length + 1][];
 
             for (int i = 0; i < bArr.Length; i++) {
@@ -24,19 +25,24 @@ namespace Matcher.Business
 
             // loop over truth table (skip first row)
             // if we encounter a '.' or char is the same, current bool = previous(j-1, i-1) result
-            //  if we encounter a '*', then current bool = previous(j - 2) character's result and
+            // if we encounter a '*', then current bool = previous(j - 2) character's result and
             //      if previous previous character was a '.' or it was equal to previous character:
             //          current character = true if current character is true, or previous(i-1) character was true
             // else the current character is false.
+
             for (int i = 1; i < bArr.Length; i++) {
                 for (int j = 1; j < bArr[0].Length; j++) {
                     if (p[j - 1] == '.' || p[j - 1] == s[i - 1]) {
                         bArr[i][j] = bArr[i - 1][j - 1];
-                    } else if (p[j - 1] == '*') {
+                    }
+
+                    else if (p[j - 1] == '*') {
                         bArr[i][j] = bArr[i][j - 2];
+
                         if (p[j - 2] == '.' || p[j - 2] == s[i - 1]) {
                             bArr[i][j] = bArr[i][j] | bArr[i - 1][j];
                         }
+
                     } else {
                         bArr[i][j] = false;
                     }
@@ -46,21 +52,20 @@ namespace Matcher.Business
             return bArr[s.Length][p.Length];
         }
 
+        // Recursive
         public static bool IsMatchRecursive(string s, string p) {
             return IsMatchR(s, p, 0, 0);
         }
 
         public static bool IsMatchR(string s, string p, int i, int j) {
-            if (j >= p.Length) {
-                return i >= s.Length && j >= p.Length;
-            }
+            // len check
+            if (j >= p.Length) return i >= s.Length && j >= p.Length;
 
             if (j + 1 < p.Length && p[j + 1] == '*') {
                 while (i < s.Length && (s[i] == p[j] || p[j] == '.')) {
                     if (IsMatchR(s, p, i, j + 2)) {
                         return true;
                     }
-
                     i++;
                 }
                 return IsMatchR(s, p, i, j + 2);
